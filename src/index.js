5cdc36444d6458b513b1de6ce1fca8b8c5d64663
kim1210 <69778811+kim1210@users.noreply.github.com>
2022-01-29 16:58:02 +0900

    Divide Network(class Youtube) for Dependency Injection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import Youtube from "./network/youtube";
+
+// ⭐⭐⭐ youtube Class를 여기서 만들어줘야 한번만 만들어진다. (App 또는 Componnet 안에서 만들지 않을 것!) ⭐⭐⭐
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
